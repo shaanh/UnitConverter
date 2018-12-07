a3c67cb46c34c807fd62e530052f17bfdb37aec4
Shaan Hoda <shaan.hoda@gmail.com>
2018-12-07 12:55:21 -0500

    -Project complete

--- routes/index.js
+++ routes/index.js
@@ -13,8 +13,22 @@ router.get('/', function(req, res, next) {
     });
 
 
+    function guid() {
+        function s4() {
+            return Math.floor((1 + Math.random()) * 0x10000)
+                .toString(16)
+                .substring(1);
+        }
+        return s4() + s4() + '-' + s4() + '-' + s4() + '-' + s4() + '-' + s4() + s4() + s4();
+    }
+    var uuid = guid();
     var info = admin.database().ref().child('numbersnunits');
-    var message = {text: 'Another user checking in!', timestamp: new Date().toString()};
+    var currentuser = admin.auth().currentUser;
+    var message = {
+        text: 'Another user checking in!',
+        userID: uuid,
+        timestamp: new Date().toString()
+    };
     var messageRef = info.push(message);
 });
 
